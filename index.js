ca56a8cbc6fa9cab61ec1648262bc7d8a49b6c9c
Jonathas <jonathass5678@gmail.com>
2021-09-03 14:29:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ function carregar_janela (){
     const janela = new BrowserWindow({
         width: 250,
         heigh: 250,
-        webPreferences: {
-            nodeIntegration: true
-          },
           frame: false,
           titleBarStyle: "customButtonsOnHover",
           transparent: true,
           alwaysOnTop: true,
           resizable: false,
-          movable:true,
-
-          
+          movable: true,
+          icon: __dirname + "/icon.png",
     })
 
     janela.loadFile('index.html');
